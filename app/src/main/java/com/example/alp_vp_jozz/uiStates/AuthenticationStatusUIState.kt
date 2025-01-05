0771cd870aec54da75b5d3280d4314267bec852d
Alkupa01 <albertkurniawan383@gmail.com>
2025-01-05 16:38:31 +0700

    siip

---
+++ app/src/main/java/com/example/alp_vp_jozz/uiStates/AuthenticationStatusUIState.kt
@@ -0,0 +1,8 @@
+package com.example.alp_vp_jozz.uiStates
+
+sealed interface AuthenticationStatusUIState {
+    data class Success(val userModelData: UserModel): AuthenticationStatusUIState
+    object Loading: AuthenticationStatusUIState
+    object Start: AuthenticationStatusUIState
+    data class Failed(val errorMessage: String): AuthenticationStatusUIState
+}

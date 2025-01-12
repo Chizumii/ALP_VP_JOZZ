04896f25570b4c653fbdd68ddca2694fbfb01c8b
OttimonieS <itsdont200@gmail.com>
2025-01-12 21:11:16 +0700

    tes

---
+++ app/src/main/java/com/example/alp_vp_jozz/repositories/ProfileRepository.kt
@@ -0,0 +1,84 @@
+package com.example.alp_vp_jozz.repositories
+
+import com.example.alp_vp_jozz.models.UserResponse
+import com.example.alp_vp_jozz.services.ProfileService
+import retrofit2.Call
+
+interface ProfileRepository {
+    fun getUserProfile(token: String, id: Int): Call<UserResponse>
+
+    fun createUserProfile(token: String, username: String?, email: String?, password: String?, namaDepan: String?, namaBelakang: String?, nomorTelp: String?, nicknameGame: String?, teamID: Int?): Call<UserResponse>
+
+    fun updateUserProfile(token: String, id: Int, username: String?, email: String?, password: String?, namaDepan: String?, namaBelakang: String?, nomorTelp: String?, nicknameGame: String?, teamID: Int?): Call<UserResponse>
+
+    fun deleteUserProfile(token: String, id: Int): Call<UserResponse>
+}
+
+class NetworkProfileRepository(
+    private val profileService: ProfileService
+) : ProfileRepository {
+
+    override fun getUserProfile(token: String, id: Int): Call<UserResponse> {
+        return profileService.getUserProfile("Bearer $token", id)
+    }
+
+    override fun createUserProfile(
+        token: String,
+        username: String?,
+        email: String?,
+        password: String?,
+        namaDepan: String?,
+        namaBelakang: String?,
+        nomorTelp: String?,
+        nicknameGame: String?,
+        teamID: Int?
+    ): Call<UserResponse> {
+        return profileService.createUserProfile(
+            token,
+            UserResponse(
+                username = username ?: "",
+                email = email ?: "",
+                password = password ?: "",
+                nama_depan = namaDepan ?: "",
+                nama_belakang = namaBelakang ?: "",
+                nomor_telp = nomorTelp ?: "",
+                nicknamegame = nicknameGame ?: "",
+                teamid = teamID ?:0,
+                token = TODO(),
+            )
+        )
+    }
+
+    override fun updateUserProfile(
+        token: String,
+        id: Int,
+        username: String?,
+        email: String?,
+        password: String?,
+        namaDepan: String?,
+        namaBelakang: String?,
+        nomorTelp: String?,
+        nicknameGame: String?,
+        teamID: Int?
+    ): Call<UserResponse> {
+        return profileService.updateUserProfile(
+            token,
+            id,
+            UserResponse(
+                username = username ?: "",
+                email = email ?: "",
+                password = password ?: "",
+                nama_depan = namaDepan ?: "",
+                nama_belakang = namaBelakang ?: "",
+                nomor_telp = nomorTelp ?: "",
+                nicknamegame = nicknameGame ?: "",
+                teamid = teamID ?: 0,
+                token = TODO()
+            )
+        )
+    }
+
+    override fun deleteUserProfile(token: String, id: Int): Call<UserResponse> {
+        return profileService.deleteUserProfile("Bearer $token", id)
+    }
+}
